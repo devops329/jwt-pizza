6923e57157e5bd6b1a9579bc2cbc7ced68922855
Ethan Superior <evan.k.chase@gmail.com>
2024-10-10 18:18:05 -0600

    Fixed the great fat fingered bug convig of 2024

--- playwright.config.js
+++ playwright.config.js
@@ -2,10 +2,10 @@ import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
 	testDir: "./tests",
-	fullyParallel: true,
+	fullyParallel: false,
 	forbidOnly: !!process.env.CI,
 	retries: process.env.CI ? 2 : 0,
-	workers: process.env.CI ? 1 : undefined,
+	workers: process.env.CI ? 1 : 1,
 	reporter: "html",
 	timeout: 3000,
 	use: {
