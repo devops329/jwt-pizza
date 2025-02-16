31324def4daa3a604d3c62763fe496bc0d0f5aaf
JonahKunzler <jonahk12@gmail.com>
2025-02-15 18:44:01 -0700

    test 2

--- tests/pizza.spec.js
+++ tests/pizza.spec.js
@@ -1,12 +1,11 @@
 import { test, expect } from 'playwright-test-coverage';
 
 test('home page', async ({ page }) => {
-  await page.go
-  await page.goto('chrome-error://chromewebdata/');to('/');
+  await page.goto('/');
 
   expect(await page.title()).toBe('JWT Pizza');
 });
 
 test('buy pizza with login', async ({ page }) => {
-  
+  await page.goto('http://localhost:5173/');
 });
