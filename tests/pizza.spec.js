e6a0089a5ec0fdcf78157c853fb37453d7a38102
JonahKunzler <jonahk12@gmail.com>
2025-02-15 19:19:36 -0700

    workflow complete

--- tests/pizza.spec.js
+++ tests/pizza.spec.js
@@ -7,5 +7,14 @@ test('home page', async ({ page }) => {
 });
 
 test('buy pizza with login', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
+  await page.getByRole('button', { name: 'Order now' }).click();
+  await page.getByRole('combobox').selectOption('4');
+  await page.getByRole('link', { name: 'Image Description Veggie A' }).click();
+  await page.getByRole('link', { name: 'Image Description Pepperoni' }).click();
+  await page.getByRole('button', { name: 'Checkout' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('d@jwt.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('d');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('button', { name: 'Pay now' }).click();
 });
